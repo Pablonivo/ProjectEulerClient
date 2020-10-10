988fc9a2f4a580afbb0c93b833a216d729da2fff
Pablonivo <ivokok1993@gmail.com>
2020-10-10 16:23:15 +0200

    Added computation time to cards & added olutions 19 & 20

--- src/app/components/solution-card/solution-card.component.ts
+++ src/app/components/solution-card/solution-card.component.ts
@@ -16,31 +16,39 @@ export class SolutionCardComponent implements OnInit, OnDestroy {
   problemTitle: string;
   isComputing: boolean = false;
 
+  _startTime: number;
+  _endTime: number;
+  _numberOfSecondsUsedForLastComputation: number;
+
   @Input()
   problemId: number;
 
   constructor(
-      private readonly _solutionService: SolutionService,
-      private readonly _changeDetectorRef: ChangeDetectorRef,
-      private readonly _problemTitleService: ProblemTitleService){
+    private readonly _solutionService: SolutionService,
+    private readonly _changeDetectorRef: ChangeDetectorRef,
+    private readonly _problemTitleService: ProblemTitleService) {
   }
 
   ngOnInit(): void {
     this.problemTitle = this._problemTitleService.getProblemTitleById(this.problemId);
   }
-  
+
   ngOnDestroy(): void {
     this._subscription.unsubscribe();
   }
 
   onCompute(): void {
+    this.solution = null;
     this.isComputing = true;
+    this._startTime = performance.now();
     this._changeDetectorRef.detectChanges();
 
     this._subscription.add(
       this._solutionService.getSolutionById(this.problemId).subscribe(
         solution => {
           this.solution = solution;
+          this._endTime = performance.now();
+          this._numberOfSecondsUsedForLastComputation = Math.round(this._endTime - this._startTime) / 1000;
           this.isComputing = false;
           this._changeDetectorRef.detectChanges();
         }
